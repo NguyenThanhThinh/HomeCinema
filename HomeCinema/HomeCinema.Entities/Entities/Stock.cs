a79de518fbd5fc86fa179876127b73f6def449f5
NguyenThanhThinh <thanhthinhcntt@gmail.com>
2017-02-26 20:50:06 +0700

    add repo

---
+++ HomeCinema/HomeCinema.Entities/Entities/Stock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeCinema.Entities.Entities
+{
+   public class Stock:IEntityBase
+    {
+        public int Id { get; set; }
+        public int MovieId { get; set; }
+        public virtual Movie Movie { get; set; }
+        public Guid UniqueKey { get; set; }
+        public bool IsAvailable { get; set; }
+        public virtual ICollection<Rental> Rentals { get; set; }
+    }
+}
