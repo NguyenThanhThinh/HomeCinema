36286e07211697b078a86316d50f43e8309216bc
NguyenThanhThinh <thanhthinhcntt@gmail.com>
2017-02-26 22:39:29 +0700

    update

---
+++ HomeCinema/HomeCinema.Web/Infrastructure/Core/DataRepositoryFactory.cs
@@ -0,0 +1,28 @@
+﻿using HomeCinema.Data.Repositories;
+using HomeCinema.Web.App_Start;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Autofac;
+using System.Web.Http;
+using HomeCinema.Web.Infrastructure.Extensions;
+using System.Net.Http;
+using HomeCinema.Entities;
+using HomeCinema.Entities.Entities;
+
+namespace HomeCinema.Web.Infrastructure.Core
+{
+    public class DataRepositoryFactory : IDataRepositoryFactory
+    {
+        public IEntityBaseRepository<T> GetDataRepository<T>(HttpRequestMessage request) where T : class, IEntityBase, new()
+        {
+            return request.GetDataRepository<T>();
+        }
+    }
+
+    public interface IDataRepositoryFactory
+    {
+        IEntityBaseRepository<T> GetDataRepository<T>(HttpRequestMessage request) where T : class, IEntityBase, new();
+    }
+}
